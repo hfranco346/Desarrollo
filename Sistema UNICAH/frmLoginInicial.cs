6f6dca803621cb2d6a0cf7816830763fc0279e9a
hfranco346 <hfranco346@gmail.com>
2015-11-25 08:24:35 -0600

    Creacion de frmLoginInicial

---
+++ Sistema UNICAH/frmLoginInicial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MaterialSkin;
+using MaterialSkin.Controls;
+namespace Sistema_UNICAH
+{
+    public partial class frmLoginInicial : MaterialForm
+    {
+        private readonly MaterialSkinManager materialSkinManager;
+        public frmLoginInicial()
+        {
+            InitializeComponent();
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightBlue900, Primary.Grey400, Primary.Grey400, Accent.Yellow700, TextShade.WHITE);
+        }
+
+        private void pbPantalla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLoginInicial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCuenta_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtContrasena_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOlvideMiContrasena_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
